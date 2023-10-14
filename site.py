f3c1017d217639d590ceb9a3d23f3c49ead25bfa
anonymoum9282818 <147884051+anonymoum9282818@users.noreply.github.com>
2023-10-14 12:35:55 -0300

    Create site.py

---
+++ site.py
@@ -0,0 +1,6 @@
+import os
+import time
+os.system("termux-open --view https://www.invertexto.com/codigo-binario")
+input("")
+os.system("termux-open --view https://www.invertexto.com/texto-criptografado")
+input("")
